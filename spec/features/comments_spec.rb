8d328fa4c28fabc07d617ee67b3a30305f31dedd
Yanran Lu & Ben Henderson <luyanran@yahoo.com>
2014-11-03 18:02:33 -0800

    initiate comment spec

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -5,7 +5,6 @@ feature "Comments" do
   cat_user = User.find_by(username: user.username)
   cat_user.password = user.password
 
-
   user = FactoryGirl.build(:dog_user)
   dog_user = User.find_by(username: user.username)
   dog_user.password = user.password
@@ -17,7 +16,7 @@ feature "Comments" do
   
     goal = FactoryGirl.build(:private_goal)
     goal.user_id = cat_user.id
-    goal.save
+    goal.save   
   end
 
   let(:cat_user) { cat_user }
@@ -27,6 +26,8 @@ feature "Comments" do
   let(:public_goal) { cat_user.goals.where(status: "PUBLIC").first }
  
   let(:private_goal) { cat_user.goals.where(status: "PRIVATE").first }
+  
+    let(:private_goal) { cat_user.goals.where(status: "PRIVATE").first }
   
   before(:each) do
     sign_in(cat_user)
@@ -41,9 +42,13 @@ feature "Comments" do
   end
   
   feature "On Goals" do
-    scenario "comment form appears on Goal show page" do
+    
+    scenario "comment form should appear on goal show page" do
       visit goal_url(public_goal)
+      expect(page).to have_content("Create New Comment")
+      expect(page).to have_button("Create New Comment")
     end
+    
   end
   
   feature "On Users" do
